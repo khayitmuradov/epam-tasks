8ba729eb8cb1d58b13476b4043a132858809c68d
khayitmuradov <khayitmuradov@proton.me>
2023-02-10 14:34:23 +0500

    created 2 programs

---
+++ BasicOf.NETFrameworkAndCSharp/Program.cs
@@ -0,0 +1,10 @@
+﻿#nullable disable
+
+Console.Write("Enter an decimal: ");
+decimal num = decimal.Parse(Console.ReadLine());
+int intNum = Decimal.ToInt32(num);
+
+Console.WriteLine(intNum);
+
+// With 2 to 20 base number system, I have no idea what to do. Should I create this, should I use a base system called Vigesimal
+// 
