c0532e5da571f9fcf77b6e3a0b19f19e37dc74fb
khayitmuradov <khayitmuradov@proton.me>
2023-02-10 22:11:03 +0500

    update again

--- CleanCode/Program.cs
+++ CleanCode/Program.cs
@@ -1,4 +1,5 @@
 ﻿// Just as a heads up I should tell you that I rewrote
 // every program's (BasicsOfDotnetFrameworkAndCSharp, DevelopmentAndBuildTools, InterfacesAndAbstractClasses, OOP)
 // codes in order to fit coding standarts and code principles
-// you can see them by clicking on them
+// you can see and check all of them by clicking on them
+// I checked and changed every single details of the code
