8533cf020b447965c7aab632bd6931832284ae63
khayitmuradov <khayitmuradov@proton.me>
2023-02-10 20:54:50 +0500

    updated dev and build tools

--- DevelopmentAndBuildTools/Program.cs
+++ DevelopmentAndBuildTools/Program.cs
@@ -4,42 +4,27 @@ public class Program
 {
     public static void Main(string[] args)
     {
-        string? chars = Console.ReadLine();
+        Console.WriteLine("Enter the string: ");
+        string? input = Console.ReadLine();
 
-        Console.Write(ConsecutiveCharacters(chars));
-    }
+        int maxLength = 0;
+        int currentLength = 1;
 
-    public static int ConsecutiveCharacters(string characters)
-    {
-        int length = characters.Length;
-        int max = Int32.MinValue;
-        int count = 1;
-
-        for (int i = 0; i < length - 1; i++)
+        for (int i = 1; i < input.Length; i++)
         {
-            if (characters[i] != characters[length - 1])
+            if (input[i] != input[i - 1])
             {
-                count++;
-                max = Math.Max(max, count);
-            }
-            else if (characters[i] == characters[length - 1])
-            {
-                count -= 1;
-                max = Math.Max(max, count);
+                currentLength++;
             }
             else
             {
-                count = 1;
+                maxLength = Math.Max(maxLength, currentLength);
+                currentLength = 1;
             }
         }
 
-        if (max > count)
-        {
-            return max;
-        }
-        else
-        {
-            return count;
-        }
+        maxLength = Math.Max(maxLength, currentLength);
+
+        Console.WriteLine("The maximum number of unequal consecutive characters is: " + maxLength);
     }
 }
---
+++ InterfacesAndAbstractClasses/Program.cs
@@ -0,0 +1,17 @@
+﻿struct Coordinate
+{
+    int x, y, z;
+}
+
+public class Program
+{
+    public static void Main()
+    {
+
+    }
+
+    public static void Sup()
+    {
+
+    }
+}
