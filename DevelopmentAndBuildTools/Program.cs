a5b8bc69f069b7be3add07f8ab9783bbc41459cf
khayitmuradov <khayitmuradov@proton.me>
2023-02-10 09:45:08 +0500

    Add project files.

---
+++ DevelopmentAndBuildTools/Program.cs
@@ -0,0 +1,45 @@
+﻿namespace DevelopmentAndBuildTools;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        string? chars = Console.ReadLine();
+
+        Console.Write(ConsecutiveCharacters(chars));
+    }
+
+    public static int ConsecutiveCharacters(string characters)
+    {
+        int length = characters.Length;
+        int max = Int32.MinValue;
+        int count = 1;
+
+        for (int i = 0; i < length - 1; i++)
+        {
+            if (characters[i] != characters[length - 1])
+            {
+                count++;
+                max = Math.Max(max, count);
+            }
+            else if (characters[i] == characters[length - 1])
+            {
+                count -= 1;
+                max = Math.Max(max, count);
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+
+        if (max > count)
+        {
+            return max;
+        }
+        else
+        {
+            return count;
+        }
+    }
+}
