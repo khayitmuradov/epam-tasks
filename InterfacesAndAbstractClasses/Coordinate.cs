415955d4cd8be5e68d3648b55d08205b4aeabe84
khayitmuradov <khayitmuradov@proton.me>
2023-02-24 18:44:00 +0500

    created field validation

--- InterfacesAndAbstractClasses/Coordinate.cs
+++ InterfacesAndAbstractClasses/Coordinate.cs
@@ -1,15 +1,61 @@
 ﻿namespace InterfacesAndAbstractClasses;
-
 public struct Coordinate
 {
-    public int X { get; set; }
-    public int Y { get; set; }
-    public int Z { get; set; }
+    private int x;
+    private int y;
+    private int z;
 
     public Coordinate(int x, int y, int z)
     {
-        X = x;
-        Y = y;
-        Z = z;
+        if (x < 0 || y < 0 || z < 0)
+        {
+            throw new ArgumentException("Coordinate values cannot be negative.");
+        }
+
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public int X
+    {
+        get { return x; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Coordinate value cannot be negative.");
+            }
+
+            x = value;
+        }
+    }
+
+    public int Y
+    {
+        get { return y; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Coordinate value cannot be negative.");
+            }
+
+            y = value;
+        }
+    }
+
+    public int Z
+    {
+        get { return z; }
+        set
+        {
+            if (value < 0)
+            {
+                throw new ArgumentException("Coordinate value cannot be negative.");
+            }
+
+            z = value;
+        }
     }
 }
