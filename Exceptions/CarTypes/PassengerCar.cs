89786512f9864a5e9174c63fa6813f8dbfd9ae16
khayitmuradov <khayitmuradov@proton.me>
2023-03-05 16:13:23 +0500

    exceptions changed:
    passengercar.cs
    scooter.cs
    truck.cs created

--- Exceptions/CarTypes/PassengerCar.cs
+++ Exceptions/CarTypes/PassengerCar.cs
@@ -1,12 +1,24 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Exceptions.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Exceptions.CarTypes;
+namespace Exceptions.CarTypes;
 
-//public class PassengerCar : Vehicle
-//{
+public class PassengerCar : Vehicle
+{
+    public string CarModel { get; set; }
 
-//}
+	public PassengerCar(double engineCapacity, string engineType, string serialNumber, int powerRating, string carModel)
+		: base(engineCapacity, engineType, serialNumber, powerRating)
+	{
+		if (string.IsNullOrEmpty(carModel))
+		{
+			throw new InitializationException("Car Model is empty.");
+        }
+
+		CarModel = carModel;
+    }
+}
--- Exceptions/CarTypes/Scooter.cs
+++ Exceptions/CarTypes/Scooter.cs
@@ -1,11 +1,24 @@
-﻿//using System;
-//using System.Collections.Generic;
-//using System.Linq;
-//using System.Text;
-//using System.Threading.Tasks;
+﻿using Exceptions.Exceptions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-//namespace Exceptions.CarTypes;
+namespace Exceptions.CarTypes;
 
-//public class Scooter : Vehicle
-//{
-//}
+public class Scooter : Vehicle
+{
+    public string CarModel { get; set; }
+
+	public Scooter(double engineCapacity, string engineType, string serialNumber, int powerRating, string carModel)
+		: base(engineCapacity, engineType, serialNumber, powerRating)
+	{
+		if (string.IsNullOrEmpty(carModel))
+		{
+			throw new InitializationException("Car Model is empty.");
+		}
+
+		CarModel = carModel;
+	}
+}
