b965aab3c5eaaf93c337ffa3dcd9a321482d145c
khayitmuradov <khayitmuradov@proton.me>
2023-03-11 16:51:14 +0500

    v1

---
+++ UnitTestFrameworks.ClassLib/StringUtilities.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTestFrameworks.ClassLib;
+
+public class StringUtilities
+{
+    public static int GetMaxUnequalConsecutiveChars(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return 0;
+        }
+
+        int max = 0, count = 0;
+
+        for (int i = 1; i < str.Length; i++)
+        {
+            if (str[i] != str[i - 1])
+            {
+                count++;
+                if (count > max)
+                {
+                    max = count;
+                }
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+        return max;
+    }
+
+    public static int GetMaxConsecutiveLetters(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return 0;
+        }
+
+        int max = 1, count = 1;
+        for (int i = 1; i < str.Length; i++)
+        {
+            if (char.IsLetter(str[i]) && str[i] == str[i - 1])
+            {
+                count++;
+                if (count > max)
+                {
+                    max = count;
+                }
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+        return max;
+    }
+
+    public static int GetMaxConsecutiveDigits(string str)
+    {
+        if (string.IsNullOrEmpty(str))
+        {
+            return 0;
+        }
+
+        int max = 1, count = 1;
+        for (int i = 1; i < str.Length; i++)
+        {
+            if (char.IsDigit(str[i]) && str[i] == str[i - 1])
+            {
+                count++;
+                if (count > max)
+                {
+                    max = count;
+                }
+            }
+            else
+            {
+                count = 1;
+            }
+        }
+        return max;
+    }
+}
---
+++ UnitTestFrameworks.Test/StringUtilsTests.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using UnitTestFrameworks.ClassLib;
+
+namespace UnitTestFrameworks.Test;
+
+[TestFixture]
+public class StringUtilsTests
+{
+    private StringUtilities _stringUtilities;
+
+    [SetUp]
+    public void Setup()
+    {
+        _stringUtilities = new StringUtilities();
+    }
+
+
+    [Test]
+    public void GetMaxUnequalConsecutiveChars_WhenInputIsEmpty_ReturnsZero()
+    {
+        // Arramge
+        string? input = "";
+
+        // Act
+        //int result = StringUtilities.
+    }
+}
